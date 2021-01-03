1a2d962661b098daea9cff215e1bedb535111ca0
Abayomi Shosilva <ashosilva@gmail.com>
2021-01-03 16:46:08 -0500

    Update commit

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,18 @@
 function pairSum(nums, target) {
   // Insert code here;
-  
+  if(nums.length <= 1){
+    throw "Array too small";
+  }
+  var i = 0;
+  while(i < nums.length-1){
+    for(var j = i+1; j < nums.length; j++){
+      if((nums[i] + nums[j]) == target) {
+        return true;
+      }
+    }
+    i++
+  }
+  return false;
 }
 
 // Do not edit this line;
