45329c07ea8d0fb76b86fef5022cafa656dbe271
Abayomi Shosilva <ashosilva@gmail.com>
2021-01-04 04:47:18 -0500

    Update commit

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,20 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    var mid = Math.round(nums.length/2)-1;
+    var arrLeft = nums.slice(0,mid);
+    var arrRight = nums.slice(mid+1, nums.length);
+    
+    if(target == nums[mid]){
+      return true;
+    }else if(target > nums[mid] && arrRight.length > 0){
+      return this.binarySearch(arrRight,target);
+    }else if(target < nums[mid] && arrLeft.length > 0){
+      return this.binarySearch(arrLeft,target);
+    }else{
+      return false;
+    }
+   
   }
 }
 
